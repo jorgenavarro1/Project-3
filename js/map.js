3a60669f1b5d2e47cb387548d553344b44c18181
jorgenavarro1 <jnavarro11@hawk.iit.edu>
2024-04-09 22:50:00 -0500

    Adding in javascript

--- js/map.js
+++ js/map.js
@@ -1,1 +1,35 @@
+function init(){
+  var el = document.getElementById('canvas');
+  var myLocation = new google.maps.LatLng(41.837486, -87.622701);
+  var mapOptions = {
+    center: myLocation,
+    zoom: 20,
+    mapTypeId: google.maps.MapTypeId.SATELLITE,
+    mapTypeControlOptions: {
+      position: google.maps.ControlPosition.BOTTOM_CENTER
+    }
+  };
 
+  var myMap = new google.maps.Map(el, mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: myLocation,
+    map: myMap,
+    animation: google.maps.Animation.DROP,
+    icon: 'media/iitlogo.png'
+  });
+
+  var contentString = '<h1>IIT Gunsaulus Hall</h1>';
+
+  var infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+
+  google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open(myMap, marker);
+    });
+
+
+}
+
+google.maps.event.addDomListener(window, 'load', init);
